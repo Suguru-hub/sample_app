884c6399fbece52626ff83530c33df0dc5e4f262
Suguru-hub <mongos6207@icloud.com>
2020-12-29 14:09:57 +0900

    Userモデルのテストケース追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    # @userオブジェクトが有効かどうか(valid?メソッド。Userモデルにバリデーションがない段階ではテストは成功するはず)
+    assert @user.valid?
+  end
 end
