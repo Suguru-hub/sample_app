b7eaed08556c1beb6b79857dfa12af1a5925dd22
Suguru-hub <mongos6207@icloud.com>
2020-12-30 17:08:57 +0900

    Userモデルにバリデーション追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,17 @@ class UserTest < ActiveSupport::TestCase
 
   test "should be valid" do
     # @userオブジェクトが有効かどうか(valid?メソッド。Userモデルにバリデーションがない段階ではテストは成功するはず)
+    # valid?メソッドはモデルのすべてのバリデーションに通った時にtrueを返す
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?  # オブジェクトが有効でなければ成功
+  end
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
   end
 end
