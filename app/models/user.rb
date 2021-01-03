20a0c37f39a8243755d076b2bc2a551d395c4c46
Suguru-hub <mongos6207@icloud.com>
2021-01-03 15:32:53 +0900

    email属性に正規表現バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 50}
-    validates :email, presence: true, length: {maximum: 255}
+    # email用の正規表現を定数変数に格納
+    # この正規表現の詳しい意味：https://railstutorial.jp/chapters/modeling_users?version=5.1#table-valid_email_regex
+    # Ruby正規表現リファレンスマニュアル：https://docs.ruby-lang.org/ja/latest/doc/spec=2fregexp.html
+    # 以下の正規表現は、foo@bar..comのようなドットの連続を誤りとして検出できるように修正してある
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255},
+                format: {with: VALID_EMAIL_REGEX}  # 正規表現のバリデーション
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,4 +31,27 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com" # バリデーションで255を上限としたから256で検証
     assert_not @user.valid?
   end
+
+  # 有効なメールアドレスの検証(emailの正規表現バリデーションが正しく機能しているか)
+  test "email validation should accept valid addresses" do
+    # %wについて：https://qiita.com/mogulla3/items/46bb876391be07921743#3-w
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # assertメソッドの第2引数にエラーメッセージを追加している。こうすれば、どのメアドでテストが失敗したのかを特定できるよね
+      # inspectメソッドは、オブジェクトや配列などをわかりやすく文字列で返してくれるメソッド
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # 無効なメアドの検証(emailの正規表現バリデーションが正しく機能しているか)
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
