b1c9a89512a123cba4ced75413bc13a15b6980d6
Suguru-hub <mongos6207@icloud.com>
2021-01-17 21:30:29 +0900

    usersのルーティング設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  def show
+    # findメソッドは主キーに対応するレコードを取り出す
+    # usersの主キーはidだから、以下のコードはidがparams[:id]と一致するオブジェクトを取得する
+    @user = User.find(params[:id])
+    # debuggerについて：https://railstutorial.jp/chapters/sign_up?version=5.1#code-debugger
+    # 今後実行中によくわからない挙動があったら、下のようにdebuggerを差し込んで調べてみよう
+    # デバッグ用メソッドなので通常は削除・コメントアウトしておくべき
+    # debugger
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  # RESTfulなusersリソースで必要となるすべてのアクションが利用できるようになる。
+  # 詳しくは以下参照
+  # https://railstutorial.jp/chapters/sign_up?version=5.1#table-RESTful_users
+  resources :users
 end
