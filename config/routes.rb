ed15c6ae95d6decd73466c5f1afd24241636e7a0
Suguru-hub <mongos6207@icloud.com>
2021-01-22 20:11:50 +0900

    ユーザー登録失敗時のエラーメッセージ表示部分を作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # RESTfulなusersリソースで必要となるすべてのアクションが利用できるようになる。
   # 詳しくは以下参照
   # https://railstutorial.jp/chapters/sign_up?version=5.1#table-RESTful_users
+  # こちらもどうぞ：https://railsguides.jp/routing.html#crud%E3%80%81%E5%8B%95%E8%A9%9E%E3%80%81%E3%82%A2%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3
   resources :users
 end
