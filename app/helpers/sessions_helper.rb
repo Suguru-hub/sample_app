b9c9f15035cef9fbf3c01be0b5aa3723b5cb005c
Suguru-hub <mongos6207@icloud.com>
2021-02-02 21:13:48 +0900

    current_userヘルパー実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,12 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+
+    # 現在ログイン中のユーザーを返す(いる場合)
+    def current_user
+        if session[:user_id]
+            # "a ||= b" は、"a = a || b"と等価
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
 end
