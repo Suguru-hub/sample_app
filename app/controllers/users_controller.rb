007bd0246f09f19a49c9da95ff1c34e52848237e
Suguru-hub <mongos6207@icloud.com>
2021-02-19 20:05:07 +0900

    edit,updateを保護

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
   end
@@ -31,11 +32,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # beforeフィルターのcorrect_userで@user変数を定義しているため不要に。
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    # beforeフィルターのcorrect_userで@user変数を定義しているため不要に。
+    # @user = User.find(params[:id])
     if @user.update_attributes(user_params)  # user_paramsはStrong Parameters↓
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -59,7 +62,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # beforeアクション
+    ### beforeアクション ###
 
     # ログイン済みユーザーかどうか確認
     def logged_in_user
@@ -67,5 +70,11 @@ class UsersController < ApplicationController
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    # 正しいユーザーがどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -53,6 +53,11 @@ module SessionsHelper
         end
     end
 
+    # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+    def current_user?(user)
+        user == current_user
+    end
+
     # ユーザーがログインしていればtrue,その他ならfalse
     def logged_in?
         !current_user.nil?
