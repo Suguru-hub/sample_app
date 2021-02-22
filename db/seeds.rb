37be610075c67c4fa22f7faa776b3f603920688c
Suguru-hub <mongos6207@icloud.com>
2021-02-22 13:08:22 +0900

    サンプルユーザー生成スクリプト実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# <DB上にサンプルユーザーを生成するRailsタスク>
+# Example Userという名前とメールアドレスを持つ1人のユーザと、
+# それらしい名前とメールアドレスを持つ99人のユーザーを作成
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
