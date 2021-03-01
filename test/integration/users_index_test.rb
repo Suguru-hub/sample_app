d2a4371c2995f0a684d937d13801a388d5e56ff4
Suguru-hub <mongos6207@icloud.com>
2021-03-01 21:17:13 +0900

    ユーザー削除のテスト実装

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,18 +2,31 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  # ページネーションを含めたUsersIndexのテスト
-  test "index including pagination" do
-    log_in_as(@user)
+  # ページネーションを含めたUsersIndexと、削除リンクとユーザー削除に対するテスト
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2   # paginationクラスを持ったdivタグがあるかチェック
-    # 最初のページにユーザーがいることを確認
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
+    assert_difference 'User.count', -1 do  # ユーザー削除後、ちゃんとユーザー数が一人減っているか
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
