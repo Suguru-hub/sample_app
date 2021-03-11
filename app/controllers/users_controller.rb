4cd3f7a0e1bde0bf1872582e8f8706b9ee75912f
Suguru-hub <mongos6207@icloud.com>
2021-03-11 20:45:50 +0900

    ユーザー登録にアカウント有効化を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,10 +28,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user  # log_inメソッドはsessions_helperで定義した
-      flash[:success] = "Welcome to the Sample App!"
-      # 新規作成したユーザのプロフィールページへリダイレクト
-      redirect_to @user # redirect_to "/users/#{@user.id}" と等価
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
