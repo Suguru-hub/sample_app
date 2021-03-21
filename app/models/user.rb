306a582a96ab304e103789e553615cd74e10e854
Suguru-hub <mongos6207@icloud.com>
2021-03-21 15:25:40 +0900

    パスワード再設定コントローラのcreateアクション実装、Userモデルにパスワード再設定用メソッド実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # 仮の属性。DBのUserはこれらの値を実際には持っていない。
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     # ユーザをDBに保存する前にemailを小文字に変換
     # いくつかのDBのアダプタが、常に大文字小文字を区別するインデックスを使っているとは限らない問題への対処
@@ -77,6 +77,18 @@ class User < ApplicationRecord
     # 有効化用のメールを送信する
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # パスワード再設定の属性を設定する
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # パスワード再設定のメールを送信する
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
     end
 
     private
