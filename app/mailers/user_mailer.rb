affc8ea3f71373b956dc1765899dd5823279f070
Suguru-hub <mongos6207@icloud.com>
2021-03-22 15:59:09 +0900

    パスワード再設定のリンクをメール送信するメソッド実装

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,14 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
+  # ユーザー有効化のリンクをメール送信する
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  # パスワード再設定のリンクをメール送信する
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"  # subjectは送信メールの件名
   end
 end
