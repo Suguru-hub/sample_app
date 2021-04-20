2f8df7dbf1c24a0144190bd45d1ae735865a5f32
Suguru-hub <mongos6207@icloud.com>
2021-04-20 19:42:56 +0900

    micropostモデルにバリデーションを追加し、テストも実装

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  # user_id属性の存在性のテスト
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  # content属性の存在性のテスト
+  test "content should be present" do
+    @micropost.content = "     "
+    assert_not @micropost.valid?
+  end
+
+  # content属性の文字数制限のテスト
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
